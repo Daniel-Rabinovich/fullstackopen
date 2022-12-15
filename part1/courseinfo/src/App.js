309fb0419c979ef68fb9660984b5c39140db9860
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-15 12:37:18 +0200

    added/removed div tags to match example

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,7 +13,7 @@ const Header = (props) => {
 // Content component
 const Content = (props) => {
   return (
-    <div>
+    <>
       <p>
         {props.part1} {props.exercises1}
       </p>
@@ -23,18 +23,18 @@ const Content = (props) => {
       <p>
         {props.part3} {props.exercises3}
       </p>
-    </div>
+    </>
   )
 }
 
 // Total component
 const Total = (props) => {
   return (
-    <div>
+    <>
       <p>
         Number of exercises {props.total}
       </p>
-    </div>
+    </>
   )
 
 }
@@ -50,7 +50,7 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <>
+    <div>
       <Header course={course} />
       <Content
         part1={part1}
@@ -61,7 +61,7 @@ const App = () => {
         exercises3={exercises3}
       />
       <Total total={exercises1+exercises2+exercises3} />
-    </>
+    </div>
   )
 }
 
