b9361a11874fb4f8da02a77e13166ef51436fc8d
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-16 16:03:29 +0200

    part 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 
-const Person = (props) => <li>{props.name} {props.number}</li>
+const Person = (props) => {
+  if (props.show){
+    return <li>{props.name} {props.number}</li>
+  } else {
+    return <></>
+  }
+}
+
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1, show:true },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2, show:true },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3, show:true },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, show:true }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
 
   const addNewPerson = (e) => {
     e.preventDefault()
@@ -24,7 +30,8 @@ const App = () => {
       const personObject = {
         name: newName,
         number: newNumber,
-        id: persons.length+1
+        id: persons.length+1,
+        show: true
       }
       setPersons(persons.concat(personObject))
     }
@@ -40,9 +47,29 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleSearchChange = (e) => {
+    const newSearch = e.target.value.toLowerCase()
+    const newPersons = persons.reduce((list, person)=>{
+      const newPerson = {
+        ...person,
+        show: person.name.toLowerCase().includes(newSearch)
+      }
+      return list.concat(newPerson)
+    },[])
+    setPersons(newPersons)
+  }
+
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <form>
+        <div>
+          filter showen with:
+          <input onChange={handleSearchChange}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName}
@@ -59,7 +86,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Person key={person.id.toString()} name={person.name} number={person.number}/>
+          <Person key={person.id.toString()} name={person.name} number={person.number} show={person.show}/>
         )}
       </ul>
     </div>
