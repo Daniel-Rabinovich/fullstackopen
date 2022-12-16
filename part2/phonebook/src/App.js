ba0afec2a7c48bf30300982dd2818d12474d3da8
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-16 12:15:11 +0200

    part 2.8 fixes

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 
-const Person = (props) => <li>{props.name} {props.phone}</li>
+const Person = (props) => <li>{props.name} {props.number}</li>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '123456789'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 
   const addNewPerson = (e) => {
@@ -23,20 +23,21 @@ const App = () => {
     } else {
       const personObject = {
         name: newName,
-        phone: newPhone
+        number: newNumber,
+        id: persons.length+1
       }
       setPersons(persons.concat(personObject))
     }
     setNewName('')
-    setNewPhone('')
+    setNewNumber('')
   }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
-  const handlePhoneChange = (e) => {
-    setNewPhone(e.target.value)
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -48,8 +49,8 @@ const App = () => {
           onChange={handleNameChange}/>
         </div>
         <div>
-          number: <input value={newPhone}
-          onChange={handlePhoneChange}/>
+          number: <input value={newNumber}
+          onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -58,7 +59,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Person key={person.name} name={person.name} phone={person.phone}/>
+          <Person key={person.id.toString()} name={person.name} number={person.number}/>
         )}
       </ul>
     </div>
