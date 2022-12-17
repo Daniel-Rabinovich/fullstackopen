df429d4b77f87066a3178b4ab0bc0ee8cd3cd31e
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-17 17:42:35 +0200

    part 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/filter'
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1, show:true },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2, show:true },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3, show:true },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, show:true }
-  ])
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -53,6 +50,14 @@ const App = () => {
     },[])
     setPersons(newPersons)
   }
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
 
   return (
