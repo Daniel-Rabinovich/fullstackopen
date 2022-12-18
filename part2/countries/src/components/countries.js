57c1e4446360ad434df77d55496446190848815a
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-18 12:52:19 +0200

    part 2.13+2.14

--- part2/countries/src/components/countries.js
+++ part2/countries/src/components/countries.js
@@ -1,13 +1,43 @@
+import { useState, useEffect } from 'react'
+import Country from './country'
 
 const Countries = ({ countries }) => {
 
-    return (
-        <div>
-            {countries.map(
-                (country) => <div key={country.name.common}>{country.name.common}</div>
-            )}
-        </div>
-    )
+    const [country, setCountry] = useState({})
+    const [countryChosen, setCountryChosen] = useState(false)
+
+    // grab country info and save it in a state
+    const handleClick = (e) => {
+        e.preventDefault()
+        const newCountry = countries.filter(country => {
+            return country.name.common === e.target.firstChild.data
+        })
+        setCountry(newCountry[0])
+        setCountryChosen(true)
+    }
+
+    // display the country if clicked show
+    const output = () => {
+        if(countryChosen){
+            return <Country country={country} />
+        } else {
+            return (
+                <div>
+                    {countries.map(
+                        (country, i) =>
+                        <div key={country.name.common}>
+                            <form onSubmit={handleClick}>
+                                {country.name.common}
+                                <button type="submit">show</button>
+                            </form>
+                        </div>
+                    )}
+                </div>
+            )
+        }
+    }
+
+    return output()
 }
 
 export default Countries
--- part2/countries/src/components/country.js
+++ part2/countries/src/components/country.js
@@ -1,6 +1,11 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Country = ({country}) => {
 
+    const [weather ,setWeather] = useState({})
+
+    // get the list of the languages in the country
     const extractLanguages = (country) => {
         let listLang = []
         for (let key in country.languages){
@@ -8,6 +13,21 @@ const Country = ({country}) => {
         }
         return listLang
     }
+
+    // fetch weather data once country is changed
+    useEffect(() => {
+        axios
+        .get(`https://wttr.in/${country.name.common}?format=j1`)
+        .then(response => {
+            const current = response.data.current_condition[0]
+            setWeather({
+                temp: current.temp_C,
+                wind: current.windspeedKmph
+            })
+        })
+        .catch(error => console.log("error fetching weather data", error))
+    } ,[country])
+
 
     return (
         <div>
@@ -21,13 +41,24 @@ const Country = ({country}) => {
                 area: {country.area}
             </div>
             <div>
-                <p><b>Languages</b></p>
+                <p>
+                    <b>Languages</b>
+                </p>
                 <ul>
-                {extractLanguages(country).map(lang => <li key={lang}>{lang}</li>)}
+                    {extractLanguages(country).map(lang => <li key={lang}>{lang}</li>)}
                 </ul>
             </div>
             <div>
-                <img src={country.flags.png} />
+                <img src={country.flags.png} alt={country.name.common}   />
+            </div>
+            <div>
+                <h2>Weather in {country.name.common}</h2>
+                <div>
+                    temperature: {weather.temp} C
+                </div>
+                <div>
+                    wind: {weather.wind} Kmph
+                </div>
             </div>
         </div>
     )
